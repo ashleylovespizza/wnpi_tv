891758936e5e957a44a88fbf469424672cbd9f95
ashleylovespizza <aholtgraver@gmail.com>
2017-10-26 22:22:52 -0400

    tvguide progress

--- TVGuide.js
+++ TVGuide.js
@@ -8,6 +8,7 @@
 *
 */
 var moment = require('moment');
+var getDuration = require('get-video-duration');
 
 var SPREADSHEET_ID = '1GWwkK9Phqy_kOh35o0MD36Ba97p9zW_YHf8oWEcCDl0';
 var SRC_LOCATION = "/Volumes/WNPI_SRC/wnpi/";
@@ -23,6 +24,7 @@ module.exports = class TvGuide {
         }
 
         this.channels = [];
+        this.folders = [];
         this.curr_channel = 0;
         this.initialized = false;
 
@@ -31,35 +33,76 @@ module.exports = class TvGuide {
     
 
     createTvGuide() {
-
-        instance.channels[1].print();
-
+        console.log('create tv guide');
         // assume all folders rn
+        var prevFolder = null;
+        var currFolder = null;
+        var mostRecentStart = null;
+        var mostRecentLength = null;
+
+        // first, get all folders used
         for (let i=0; i<instance.channels.length; i++) {
-            
-        }
-        for(let i in instance.channels) {
-          for (let j in instance.channels[i]) {
-            console.log(instance.channels[i][j])
-            if ('content' in instance.channels[i][j]) {
 
-              console.log(instance.channels[i][j]['content']);
-              var folderName = instance.channels[i][j]['content'];
-              FOLDERS[folderName] = [];
+            for (let j=0; j<instance.channels[i]['content'].length; j++) {
+                  
+                var line = instance.channels[i]['content'][j];
+                if (line['content'] != null) {
+                    console.log(line['content'])
+                    var folder = SRC_LOCATION + line['content'];
+                    instance.folders.push(folder);
+                    // and get its legth
+                    getDuration(folder).then((duration) => {
+                      console.log(duration);
+                    });
 
-              if (FOLDERS[folderName].length == 0) {
-                var files = fs.readdirSync(folderName);
-                for (f in files) {
-                  if ( (/\.(avi|mov|mkv|mp4)$/i).test(files[f])) {
-                    FOLDERS[folderName].push(files[f])
-                  }
 
                 }
-
-              }
-                    
             }
-          }
+           
+        }
+
+        for (let i=0; i<instance.folders.length; i++) {
+          //  console.log(instance.folders[i])
+        }
+
+
+        for (let i=0; i<instance.channels.length; i++) {
+            if (instance.channels[i]['content'] != null) {
+
+             // console.log('look into ', instance.channels[i]['content']);
+              currFolder = SRC_LOCATION + instance.channels[i]['content'];
+
+              // start at first 'content' listing, go until next (wrapping around if necessary)
+
+
+              // FOLDERS[folderName] = [];
+
+              // if (FOLDERS[folderName].length == 0) {
+              //   var files = fs.readdirSync(folderName);
+              //   for (f in files) {
+              //     if ( (/\.(avi|mov|mkv|mp4)$/i).test(files[f])) {
+              //       FOLDERS[folderName].push(files[f])
+              //     }
+
+              //   }
+
+              // }
+            }
+            if (currFolder != null) {
+                // if (prevFolder == null) {
+                //     // happening for the FIRST TIME
+                //     prevFolder  = currFolder;
+                // }
+
+                // if (mostRecentStart == null) {
+                //     // let's start something
+                // }
+                // // if (current time - most recent start time) - the lenght of whatever it thinks it's playing rn is less than 30 min)
+                // if ( ((instance.channels[i]['time'] - mostRecentStart) - mostRecentLength) < 30) {
+
+                // }
+
+            }
         }
 
 

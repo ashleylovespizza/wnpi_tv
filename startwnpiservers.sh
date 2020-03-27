263cc348b042fa7dec8f40687cc19208a50085ed
Ashley Holtgraver <ashley@Ashleys-MacBook-Pro.local>
2020-03-27 14:15:12 -0400

    only need to start the server

--- startwnpiservers.sh
+++ startwnpiservers.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /home/pi/wnpi/wnpi_tv
 forever server.js &
-cd /home/pi/wnpi/wnpi_tv/app
+#cd /home/pi/wnpi/wnpi_tv/app
 # for now don't worry about forevering
-serve -p 5001 &
+#serve -p 5001 &
 sleep 10s

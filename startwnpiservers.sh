d6b59d7fbb38c3477d15b7b64ae8e3a7e4f0303e
Ashley Holtgraver <ashley@Ashleys-MacBook-Pro.local>
2020-03-30 09:37:42 -0400

    make less insane start script

--- startwnpiservers.sh
+++ startwnpiservers.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 sleep 2s
 
-
-#if pidof -x  /home/pi/.nvm/versions/node/v8.9.4/bin/node /home/pi/wnpi/wnpi_tv/server.js > /dev/null
-#then
+if pgrep -f server.js > /dev/null then
 
   cd /home/pi/wnpi/wnpi_tv
     forever server.js &
-#else
-#    echo "WNPI server already running."
-#fi
+else
+    echo "WNPI server already running."
+fi
 
 
-
+#
 #cd /home/pi/wnpi/wnpi_tv/app
 # for now don't worry about forevering
 #serve -p 5001 &

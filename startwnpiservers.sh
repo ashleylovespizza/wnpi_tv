996e3fff074b9609868c43c85c0935a5afb4d2e5
Ashley Holtgraver <ashley@Ashleys-MacBook-Pro.local>
2020-03-29 23:23:39 -0400

    in startwnpiservers shell script, only start server if it's not already running

--- startwnpiservers.sh
+++ startwnpiservers.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 sleep 2s
-cd /home/pi/wnpi/wnpi_tv
-forever server.js &
+
+
+if pidof -x  /home/pi/.nvm/versions/node/v8.9.4/bin/node /home/pi/wnpi/wnpi_tv/server.js > /dev/null
+then
+
+  cd /home/pi/wnpi/wnpi_tv
+  forever server.js &
+else
+    echo "WNPI server already running."
+fi
+
+
+
 #cd /home/pi/wnpi/wnpi_tv/app
 # for now don't worry about forevering
 #serve -p 5001 &
-
